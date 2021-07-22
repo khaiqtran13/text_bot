236a7841538caacfd66e8d4b6fae29518612e465
Khai Tran <khaitran@Khais-MacBook-Air.local>
2021-07-22 17:37:51 -0400

    reset parameters

--- sendMessage.py
+++ sendMessage.py
@@ -16,11 +16,12 @@ def send_message(phone_number, message):
     os.system('osascript send.scpt {} "{}"'.format(phone_number, message))
 
 if __name__ == '__main__':
-    target_phone_number = '6132196977'
+    target_phone_number = ''
     words = get_words('toSend.txt')
-    # for i in range(1):
-    #     for word in words:
-    #             send_message(target_phone_number, (word)) #{phone number, text message}
-    message = "go get a job"
-    for i in range(100):
+    
+    # for word in words:
+    #         send_message(target_phone_number, word) #{phone number, text message}
+
+    message = ""
+    for i in range(5):
         send_message(target_phone_number, message) #{phone number, text message}
